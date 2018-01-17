a4bd74a10f7e50213edce8971eeb937dfc9f08ce
Brian Sweeney <bsweeney930@gmail.com>
2018-01-17 09:58:19 -0500

    basic blockchain created and tested;

--- main.js
+++ main.js
@@ -13,3 +13,31 @@ class Block{
         return SHA256(this.index +this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, '01/01/2018', "Genesis Block", "0" );
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1]
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let brianCoin = new Blockchain();
+
+
+brianCoin.addBlock(new Block(2, '1/12/2018', {amount: 4}));
+brianCoin.addBlock(new Block(1, '1/12/2018', {amount: 10}));
+
+console.log(JSON.stringify(brianCoin, null, 4))
